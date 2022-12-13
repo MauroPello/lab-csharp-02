b25ef6cadcb5769b414c315b71d0e26eee73c9e1
MauroPello <mauro.pellonara@outlook.com>
2022-12-13 22:22:58 +0100

    Finish 1-Collections

--- 1-Collections/User.cs
+++ 1-Collections/User.cs
@@ -6,8 +6,14 @@ namespace Collections
     {
         public User(string fullName, string username, uint? age)
         {
-            throw new NotImplementedException("TODO ensure that username is not null");
-            throw new NotImplementedException("TODO initialise this instance of user accordingly");
+            if(username == null)
+            {
+                throw new ArgumentNullException("Username cannot be null");
+            }
+
+            this.Age = age;
+            this.Username = username;
+            this.FullName = fullName;
         }
         
         public uint? Age { get; }
@@ -16,8 +22,20 @@ namespace Collections
         
         public string Username { get; }
 
-        public bool IsAgeDefined => throw new NotImplementedException("TODO check whether age is non-null or not");
+        public bool IsAgeDefined => Age != null;
         
-        // TODO implement missing methods (try to autonomously figure out which are the necessary methods)
+        protected bool Equals(User other) => Age == other.Age && FullName == other.FullName && Username == other.Username;
+
+        public override bool Equals(object obj)
+        {
+            if (ReferenceEquals(null, obj)) return false;
+            if (ReferenceEquals(this, obj)) return true;
+            if (obj.GetType() != this.GetType()) return false;
+            return Equals((User) obj);
+        }
+
+        public override int GetHashCode() => HashCode.Combine(Age, FullName, Username);
+
+        public override string ToString() => $"Username: {Username}, Full name: {FullName}, Age: {Age}.";
     }
 }
